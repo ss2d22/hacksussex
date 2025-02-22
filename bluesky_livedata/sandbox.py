27ee8b639d556fa0649e3626fbe2f6e6e9059e18
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2025-02-22 17:03:32 +0000

    added keywork extraction

--- bluesky_livedata/sandbox.py
+++ bluesky_livedata/sandbox.py
@@ -1,6 +1,9 @@
 import asyncio
 from websockets.asyncio.client import connect
 import json
+import yake
+
+kw_extractor = yake.KeywordExtractor()
 
 async def hello():
     async with connect("wss://jetstream2.us-east.bsky.network/subscribe?wantedCollections=app.bsky.feed.post") as websocket:
@@ -8,8 +11,9 @@ async def hello():
             message = await websocket.recv()
             json_message = json.loads(message)
             try:
-                print(json_message["commit"]["record"]["text"])
-                print(json_message)
+                #print(json_message["commit"]["record"]["text"])
+                kw = kw_extractor.extract_keywords(json_message["commit"]["record"]["text"])
+                print(kw[0] if kw else "")
             except KeyError:
                 pass
 
